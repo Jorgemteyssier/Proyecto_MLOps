883bac4148cc2f2d2834b78f1801d1e5e0117d09
Jorgemteyssier <105815528+Jorgemteyssier@users.noreply.github.com>
2024-08-21 04:20:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -196,3 +196,7 @@ def recomendacion(title: str):
 
     # Devolver las películas recomendadas
     return result
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=10000)
